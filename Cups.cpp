0eed4d87cd1646b43cbad7842e4e90d2460f6cf9
JorgaqMiholli <jorgaqmiholli8@gmail.com>
2024-06-06 20:20:52 +0200

    Add virtual fill and clean methods to Cup class

--- Cups.cpp
+++ Cups.cpp
@@ -14,4 +14,17 @@ public:
     std::string getMaterial() const { return material; }
     int getQuantity() const { return quantity; }
     void setQuantity(int q) { quantity = q; }
+
+    virtual void fill() const {
+        std::cout << "Filling a cup made of " << material << " with size " << size << ".\n";
+    }
+
+    virtual void clean() const {
+        std::cout << "Cleaning " << quantity << " cups made of " << material << " with size " << size << " by hand using hot water.\n";
+    }
 };
+
+int main()
+{
+    return 0;
+}
